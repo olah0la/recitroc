ee76c96ed9897cefa008b2f181124973a262043b
Steven <steven.ola@icm-institute.org>
2025-08-31 17:09:04 +0200

    changed the name. added to gitignore. init various files

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
@@ -9,5 +10,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
-
-
